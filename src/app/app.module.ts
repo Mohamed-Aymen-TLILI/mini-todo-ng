c24aee721e3852e69a6ce8568267a0dcafc4310f
libra1919 <aymentli@gmail.com>
2020-11-25 22:45:30 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import { TodoService } from "./todo.service";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, FlexLayoutModule, FormsModule],
+  providers: [TodoService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
